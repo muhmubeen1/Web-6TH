9ba852b472610d94b4d011333eed91e02732cb64
Muhammad Mubeen <141475267+muhmubeen1@users.noreply.github.com>
2024-02-22 17:49:56 +0000

    commit changes

---
+++ Web6th/Javascript/basic.js
@@ -0,0 +1,16 @@
+const account_Id = 1445;
+let account_email = "qHf8w@example.com";
+let account_password = "123456";
+account_city = "Lahore";
+// account_Id=2;
+console.log(account_Id);
+//account cannot be changed const cannot be changed
+account_email = "qHf8w@example.com";
+account_password = "1235";
+account_city="lahore";
+ let account_state;
+console.table({account_Id,account_email,account_password,account_city,account_state})
+let num_a =65;
+let num_b =25;
+sum =num_a+num_b;
+console.log(sum);
